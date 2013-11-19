a14cb4564d6315d4cd8b70e653a9296195b730a4
saasbook <saasbook@saasbook.(none)>
2013-11-19 07:27:19 -0800

    modified:   ../../db/migrate/20131028181935_create_posts.rb 	modified:   posts_controller.rb 	modified:   ../views/layouts/application.html.erb 	modified:   ../views/posts/edit.html.erb 	modified:   ../views/posts/index.html.erb 	deleted:    ../views/posts/index.html.haml 	modified:   ../views/posts/new.html.erb 	modified:   ../views/posts/show.html.erb 	modified:   ../../db/migrate/20131028181935_create_posts.rb

--- db/migrate/20131028181935_create_posts.rb
+++ db/migrate/20131028181935_create_posts.rb
@@ -1,6 +1,7 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
+    add_column :posts, :ss1_enabled, :boolean, :default true
       t.string :title
       t.text :content
       
